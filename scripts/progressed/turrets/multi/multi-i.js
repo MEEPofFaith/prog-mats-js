96135ac259cbcac97d46a388efac2c933201bfc9
MEEP of Faith <meepmatthew@gmail.com>
2020-12-06 14:25:25 -0800

    T1 Multi Turret Sprites and basic form

--- scripts/progressed/turrets/multi/multi-i.js
+++ scripts/progressed/turrets/multi/multi-i.js
@@ -1,37 +1,38 @@
 const multiTLib = this.global.pm.multiTurret;
 
-const t1 = multiTLib.newMount(Bullets.missileExplosive, "error");
-t1.x = 18;
-t1.shots = 3;
-t1.burstSpacing = 3;
-t1.spread = 5;
-t1.range = 15 * 8;
-t1.ejectEffect = Fx.casing2;
-t1.ammoPerShot = 0.1;
+const unoMount = multiTLib.newMount(Bullets.standardHoming, "prog-mats-unoM");
+unoMount.reloadTime = 25;
+unoMount.ammoPerShot = 5;
+unoMount.x = 2.75;
+unoMount.y = 2.75;
+unoMount.recoilAmount = 1;
+unoMount.range = 9 * 8;
 
-const t2 = multiTLib.newMount(Bullets.standardHoming, "error");
-t2.y = -18;
-t2.shots = 15;
-t2.reloadTime = 60;
-t2.burstSpacing = 1;
-t2.range = 10 * 8;
-t2.altEject = false;
-t2.ejectEffect = Fx.casing1;
-t2.ammoPerShot = 0.05;
+const hailMount = multiTLib.newMount(Bullets.artilleryDense, "prog-mats-hailM");
+hailMount.targetAir = false;
+hailMount.reloadTime = 60;
+hailMount.ammoPerShot = 20;
+hailMount.x = -3.75;
+hailMount.y = -4;
+hailMount.recoilAmount = 2.5;
+hailMount.range = 18 * 8;
 
-const t3 = multiTLib.newMount(Bullets.artilleryIncendiary, "error");
-t3.x = -18;
-t3.shots = 3;
-t3.velocityInaccuracy = 0.2;
-t3.inaccuracy = 10;
-t3.range = 45 * 8;
-t3.ammoPerShot = 0.11;
-t3.targetAir = false;
+const waveMount = multiTLib.newMount(Bullets.slagShot, "prog-mats-waveM");
+waveMount.reloadTime = 7;
+waveMount.x = 4.25;
+waveMount.y = -3.5;
+waveMount.recoilAmount = 1;
+waveMount.range = 13 * 8;
 
-const weapons = [t1, t2, t3];
+const weapons = [unoMount, hailMount, waveMount];
+
+const mainBullet = extend(BasicBulletType, {});
+mainBullet.ammoMultiplier = 45;
 
 //Jumble -> Amalgamation -> Conglomeration
-const jumble = multiTLib.newMultiTurret("multi-i", weapons, Items.copper, Bullets.standardThoriumBig);
-jumble.size = 3;
-jumble.range = 20 * 8;
-jumble.maxAmmo = 60;
+const jumble = multiTLib.newMultiTurret("multi-i", weapons, Items.copper, mainBullet, 80, 20);
+jumble.size = 2;
+jumble.range = 15 * 8;
+jumble.maxAmmo = 225;
+jumble.ammoPerShot = 12;
+jumble.recoil = 2;
