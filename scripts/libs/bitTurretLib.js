f9297cf07e2232ca3bd5b61695f36cadfea7a5fd
MEEP of Faith <meepmatthew@gmail.com>
2021-01-06 11:43:37 -0800

    Experimenting
    Alternate does not work with bit turret at all.

--- scripts/libs/bitTurretLib.js
+++ scripts/libs/bitTurretLib.js
@@ -1,13 +1,13 @@
 const tr = new Vec2();
 module.exports = {
-  new8BitTurret(name, sides, type, build){
+  new8BitTurret(name, sides, type, build, perRot){
     const bit = extendContent(type, name, {
       load(){
         this.super$load();
         this.bits = [];
         this.heats = [];
         this.outlines = [];
-        for(var i = 0; i < sides; i++){
+        for(var i = 0; i < (perRot ? sides : 1); i++){
           //Starts at 0*, goes counter-clockwise
           this.bits[i] = Core.atlas.find(this.name + "-side-" + i); //Turret
           this.heats[i] = Core.atlas.find(this.name + "-side-heat-" + i); //Heats
@@ -27,30 +27,51 @@ module.exports = {
 
           Draw.z(Layer.turret);
           
-          var drawRot = this.rotation - 90;
-          drawRot = drawRot >= 360 ? drawRot - 360 : drawRot < 0 ? drawRot + 360 : drawRot;
-          var rot = Mathf.round(drawRot + (360 / sides / 2), 360 / sides);
-          rot = rot >= 360 ? rot - 360 : rot < 0 ? rot + 360 : rot;
-          var curRot = Mathf.round(rot / (360 / sides));
-          tr.trns(rot + 90, -this.recoil);
-          tr.add(this.x, this.y);
-          
-          //Debug
-          /*for(var i = 0; i < sides; i++){
-            Lines.lineAngle(this.x, this.y, i * (360 / sides) + 90 + (360 / sides / 2), 80);
-          }*/
-          //End Debug
-          
-          Drawf.shadow(bit.bits[curRot], tr.x - (bit.size / 2), tr.y - (bit.size / 2), 0);
-          Draw.rect(bit.outlines[curRot], tr.x, tr.y, 0);
-          Draw.rect(bit.bits[curRot], tr.x, tr.y, 0);
+          if(perRot){
+            var drawRot = this.rotation - 90;
+            drawRot = drawRot >= 360 ? drawRot - 360 : drawRot < 0 ? drawRot + 360 : drawRot;
+            var rot = Mathf.round(drawRot + (360 / sides / 2), 360 / sides);
+            rot = rot >= 360 ? rot - 360 : rot < 0 ? rot + 360 : rot;
+            var curRot = Mathf.round(rot / (360 / sides));
+            tr.trns(rot + 90, -this.recoil);
+            tr.add(this.x, this.y);
+            
+            //Debug
+            /*for(var i = 0; i < sides; i++){
+              Lines.lineAngle(this.x, this.y, i * (360 / sides) + 90 + (360 / sides / 2), 80);
+            }*/
+            //End Debug
+            
+            Drawf.shadow(bit.bits[curRot], tr.x - (bit.size / 2), tr.y - (bit.size / 2), 0);
+            Draw.rect(bit.outlines[curRot], tr.x, tr.y, 0);
+            Draw.rect(bit.bits[curRot], tr.x, tr.y, 0);
 
-          if(bit.heats[curRot] != Core.atlas.find("error") && this.heat > 0.0001){
-            Draw.color(bit.heatColor, this.heat);
-            Draw.blend(Blending.additive);
-            Draw.rect(bit.heats[curRot], tr.x, tr.y, 0);
-            Draw.blend();
-            Draw.color();
+            if(bit.heats[curRot] != Core.atlas.find("error") && this.heat > 0.0001){
+              Draw.color(bit.heatColor, this.heat);
+              Draw.blend(Blending.additive);
+              Draw.rect(bit.heats[curRot], tr.x, tr.y, 0);
+              Draw.blend();
+              Draw.color();
+            }
+          }else{
+            var drawRot = this.rotation - 90;
+            drawRot = drawRot >= 360 ? drawRot - 360 : drawRot < 0 ? drawRot + 360 : drawRot;
+            var rot = Mathf.round(drawRot + (360 / sides / 2), 360 / sides);
+            rot = rot >= 360 ? rot - 360 : rot < 0 ? rot + 360 : rot;
+            tr.trns(rot + 90, -this.recoil);
+            tr.add(this.x, this.y);
+            
+            Drawf.shadow(bit.bits[0], tr.x - (bit.size / 2), tr.y - (bit.size / 2), rot);
+            Draw.rect(bit.outlines[0], tr.x, tr.y, rot);
+            Draw.rect(bit.bits[0], tr.x, tr.y, rot);
+
+            if(bit.heats[0] != Core.atlas.find("error") && this.heat > 0.0001){
+              Draw.color(bit.heatColor, this.heat);
+              Draw.blend(Blending.additive);
+              Draw.rect(bit.heats[0], tr.x, tr.y, rot);
+              Draw.blend();
+              Draw.color();
+            }
           }
         }
       });
--- scripts/progressed/turrets/pixel/pixel-i.js
+++ scripts/progressed/turrets/pixel/pixel-i.js
@@ -1,3 +1,3 @@
 const bitLib = this.global.pm.bitTurretLib;
 
-const bit = bitLib.new8BitTurret("pixel", 8, PowerTurret, PowerTurret.PowerTurretBuild);
+const bit = bitLib.new8BitTurret("pixel-i", 8, PowerTurret, PowerTurret.PowerTurretBuild, false);
