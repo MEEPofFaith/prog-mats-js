fe0d339a71df1ac611b54a0617ab98a71c81d11d
MEEP of Faith <meepmatthew@gmail.com>
2021-01-31 17:40:49 -0800

    Other device compatability

--- scripts/libs/newMountListValue.js
+++ scripts/libs/newMountListValue.js
@@ -20,25 +20,40 @@ module.exports = (weapons) => {
             w.add("[lightgray]" + Stat.inaccuracy.localized() + ": [white]" + weapon.inaccuracy + " " + StatUnit.degrees.localized());
           }
           if(weapon.range > 0){
-            this.sep(w, "[lightgray]" + Stat.shootRange.localized() + ": [white]" + Strings.fixed(weapon.range / Vars.tilesize, 1) + " " + StatUnit.blocks);
+            // this.sep(w, "[lightgray]" + Stat.shootRange.localized() + ": [white]" + Strings.fixed(weapon.range / Vars.tilesize, 1) + " " + StatUnit.blocks);
+            w.row();
+            w.add("[lightgray]" + Stat.shootRange.localized() + ": [white]" + Strings.fixed(weapon.range / Vars.tilesize, 1) + " " + StatUnit.blocks);
           }
           //this.sep(w, "[lightgray]" + Stat.ammoPerShot.localized() + ": [white]" + weapon.ammoPerShot);
-          this.sep(w, "[lightgray]" + Core.bundle.get("stat.prog-mats.ammo-shot") + ": [white]" + weapon.ammoPerShot);
-          this.sep(w, "[lightgray]" + Stat.reload.localized() + ": [white]" + Strings.autoFixed(60 / weapon.reloadTime * weapon.shots, 1));
+          // this.sep(w, "[lightgray]" + Core.bundle.get("stat.prog-mats.ammo-shot") + ": [white]" + weapon.ammoPerShot);
+          // this.sep(w, "[lightgray]" + Stat.reload.localized() + ": [white]" + Strings.autoFixed(60 / weapon.reloadTime * weapon.shots, 1));
           
-          this.sep(w, "[lightgray]" + Stat.targetsAir.localized() + ": [white]" + (!weapon.targetAir ? Core.bundle.get("no") : Core.bundle.get("yes")));
-          this.sep(w, "[lightgray]" + Stat.targetsGround.localized() + ": [white]" + (!weapon.targetGround ? Core.bundle.get("no") : Core.bundle.get("yes")));
+          // this.sep(w, "[lightgray]" + Stat.targetsAir.localized() + ": [white]" + (!weapon.targetAir ? Core.bundle.get("no") : Core.bundle.get("yes")));
+          // this.sep(w, "[lightgray]" + Stat.targetsGround.localized() + ": [white]" + (!weapon.targetGround ? Core.bundle.get("no") : Core.bundle.get("yes")));
+          
+          w.row();
+          w.add("[lightgray]" + Stat.ammoPerShot.localized() + ": [white]" + weapon.ammoPerShot);
+          w.row();
+          w.add("[lightgray]" + Core.bundle.get("stat.prog-mats.ammo-shot") + ": [white]" + weapon.ammoPerShot);
+          w.row();
+          w.add("[lightgray]" + Stat.reload.localized() + ": [white]" + Strings.autoFixed(60 / weapon.reloadTime * weapon.shots, 1));
+          
+          w.row();
+          w.add("[lightgray]" + Stat.targetsAir.localized() + ": [white]" + (!weapon.targetAir ? Core.bundle.get("no") : Core.bundle.get("yes")));
+          w.row();
+          w.add("[lightgray]" + Stat.targetsGround.localized() + ": [white]" + (!weapon.targetGround ? Core.bundle.get("no") : Core.bundle.get("yes")));
           
           const b = newAmmoListValue(weapon.bullet);
           b.display(w);
         }).padTop(-15).left();
         table.row();
       }
-    },
+    }/*,
     sep(table, text){
       table.row();
       table.add(text);
-    }
+    }*/
+    // Apparently attempting to call this doesn't work on some devices.
   });
   
   return w;
