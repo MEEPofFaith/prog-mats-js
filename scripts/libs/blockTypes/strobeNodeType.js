a92b22af52f6e71c5caf08da3fcc632350d84153
MEEP of Faith <meepmatthew@gmail.com>
2021-03-11 02:07:01 -0800

    h

--- scripts/libs/blockTypes/strobeNodeType.js
+++ scripts/libs/blockTypes/strobeNodeType.js
@@ -1,13 +1,13 @@
 //Haha code steal go brrrr
 const clone = obj => {
-    if(obj === null || typeof(obj) !== 'object') return obj;
-    var copy = obj.constructor();
-    for(var attr in obj) {
-        if(obj.hasOwnProperty(attr)) {
-            copy[attr] = obj[attr];
-        }
-    };
-    return copy;
+  if(obj === null || typeof(obj) !== 'object') return obj;
+  var copy = obj.constructor();
+  for(var attr in obj) {
+    if(obj.hasOwnProperty(attr)) {
+      copy[attr] = obj[attr];
+    }
+  };
+  return copy;
 }
 
 module.exports = {
