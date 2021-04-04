de02e772d925c57592315e2e6f797092a4db0515
MEEP of Faith <meepmatthew@gmail.com>
2021-04-03 18:39:31 -0700

    heresyn't

--- scripts/liquids/progressed/magma.js
+++ scripts/liquids/progressed/magma.js
@@ -1,15 +1,13 @@
-const moltenRock = extendContent(Liquid, "magma", {
+const moltenRock = extend(Liquid, "magma", {
+  flammability: 1000,
+  temperature: 1000,
+  heatCapacity: 0,
+  viscosity: 0.8,
+  color: Color.valueOf("F58859"),
+  barColor: Color.valueOf("F58859"),
+  lightColor: Color.valueOf("F58859"),
+  
   isHidden(){
     return true;
   }
 });
-moltenRock.localizedName = "Magma";
-moltenRock.description = "Actually, it's supposed to be lava...";
-moltenRock.details = "How the heck are you reading this?";
-moltenRock.flammability = 1000;
-moltenRock.temperature = 1000;
-moltenRock.heatCapacity = 0;
-moltenRock.viscosity = 0.8;
-moltenRock.color = Color.valueOf("F58859");
-moltenRock.barColor = Color.valueOf("F58859");
-moltenRock.lightColor = Color.valueOf("F58859");
